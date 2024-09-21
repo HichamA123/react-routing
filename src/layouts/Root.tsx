7ae37cbe6a0a29bca2f60bef618b48275aa3d10c
hicham <hicham_13@live.nl>
2024-09-21 21:27:43 +0200

    style: added page transitions

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,7 +1,27 @@
-import { useLocation, useOutlet } from "react-router-dom";
-import Menu from "../components/Menu";
+import { useLocation, useOutlet } from "react-router-dom"
+import Menu from "../components/Menu"
+import { AnimatePresence, motion } from "framer-motion"
+
+const pageVariants = {
+  initial: { // Starting state when component enters
+    opacity: 0,
+  },
+  animate: { // Active state (when fully visible)
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+  exit: { // Exit state when component is leaving
+    opacity: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
 
 function Root() {
+	const location = useLocation()
 	const outlet = useOutlet() // Use useOutlet to capture the children (routed components)
 
 	return (
@@ -9,7 +29,17 @@ function Root() {
 			<Menu />
 			<div className="flex justify-center mt-3">
 				<div className="basis-11/12 bg-green-500">
-						{outlet}
+					<AnimatePresence mode="wait">
+						<motion.div
+							key={location.pathname}
+							variants={pageVariants}
+							initial="initial"
+							animate="animate"
+							exit="exit"
+						>
+							{outlet}
+						</motion.div>
+					</AnimatePresence>
 				</div>
 			</div>
 		</>
